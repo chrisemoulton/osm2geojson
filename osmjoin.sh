726c98ba7738f3e68dcb1fee904616f4f1f65aaf
Peter <peathal@yahoo.de>
2013-12-08 15:22:25 +0100

    script can have custom memory

--- osmjoin.sh
+++ osmjoin.sh
@@ -1,3 +1,12 @@
 # run mvn clean install first
 # big heap needed to allow for large bucket size when sorting (less filehandles)
-java -cp target/osm2geojson-1.0-SNAPSHOT.jar:target/lib/* -Xmx1000M com.github.jillesvangurp.osm2geojson.OsmJoin "$1" | tee osmjoin.log
+JAVA=$JAVA_HOME/bin/java
+if [ "x$JAVA_HOME" = "x" ]; then
+ JAVA=java
+fi
+
+if [ "x$JAVA_OPTS" = "x" ]; then
+ JAVA_OPTS="-server -Xmx1000m"
+fi
+
+$JAVA -cp target/osm2geojson-1.0-SNAPSHOT.jar:target/lib/* $JAVA_OPTS com.github.jillesvangurp.osm2geojson.OsmJoin "$1" | tee osmjoin.log
